ba05c65245cffdd621180edfe5a4ec13d60d4430
Arka Sadhu <ark.sadhu2904@gmail.com>
2017-02-10 15:21:50 +0530

    Init tx code with uart copied

---
+++ codes/transmitter_code_vlc/Debug/uart_testing.d
@@ -0,0 +1,25 @@
+# FIXED
+
+uart_testing.obj: ../uart_testing.c
+uart_testing.obj: /home/arktheshadow/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdint.h
+uart_testing.obj: /home/arktheshadow/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdbool.h
+uart_testing.obj: /home/arktheshadow/ti/SW-EK-TM4C123GXL-2.1.3.156/inc/hw_types.h
+uart_testing.obj: /home/arktheshadow/ti/SW-EK-TM4C123GXL-2.1.3.156/inc/hw_memmap.h
+uart_testing.obj: /home/arktheshadow/ti/SW-EK-TM4C123GXL-2.1.3.156/driverlib/gpio.h
+uart_testing.obj: /home/arktheshadow/ti/SW-EK-TM4C123GXL-2.1.3.156/driverlib/uart.h
+uart_testing.obj: /home/arktheshadow/ti/SW-EK-TM4C123GXL-2.1.3.156/driverlib/pin_map.h
+uart_testing.obj: /home/arktheshadow/ti/SW-EK-TM4C123GXL-2.1.3.156/driverlib/sysctl.h
+uart_testing.obj: /home/arktheshadow/ti/SW-EK-TM4C123GXL-2.1.3.156/inc/hw_ints.h
+uart_testing.obj: /home/arktheshadow/ti/SW-EK-TM4C123GXL-2.1.3.156/driverlib/interrupt.h
+
+../uart_testing.c: 
+/home/arktheshadow/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdint.h: 
+/home/arktheshadow/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdbool.h: 
+/home/arktheshadow/ti/SW-EK-TM4C123GXL-2.1.3.156/inc/hw_types.h: 
+/home/arktheshadow/ti/SW-EK-TM4C123GXL-2.1.3.156/inc/hw_memmap.h: 
+/home/arktheshadow/ti/SW-EK-TM4C123GXL-2.1.3.156/driverlib/gpio.h: 
+/home/arktheshadow/ti/SW-EK-TM4C123GXL-2.1.3.156/driverlib/uart.h: 
+/home/arktheshadow/ti/SW-EK-TM4C123GXL-2.1.3.156/driverlib/pin_map.h: 
+/home/arktheshadow/ti/SW-EK-TM4C123GXL-2.1.3.156/driverlib/sysctl.h: 
+/home/arktheshadow/ti/SW-EK-TM4C123GXL-2.1.3.156/inc/hw_ints.h: 
+/home/arktheshadow/ti/SW-EK-TM4C123GXL-2.1.3.156/driverlib/interrupt.h: 
---
+++ codes/transmitter_code_vlc/uart_testing.c
@@ -0,0 +1,74 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/uart.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "inc/hw_ints.h"
+#include "driverlib/interrupt.h"
+
+
+
+int main()
+{
+    SysCtlClockSet(SYSCTL_SYSDIV_4  | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
+
+    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet() , 115200,
+                        (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+    IntMasterEnable();
+    IntEnable(INT_UART0);
+    UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_TX);
+
+
+
+    UARTCharPut(UART0_BASE, 'E');
+    UARTCharPut(UART0_BASE, 'n');
+    UARTCharPut(UART0_BASE, 't');
+    UARTCharPut(UART0_BASE, 'e');
+    UARTCharPut(UART0_BASE, 'r');
+    UARTCharPut(UART0_BASE, ' ');
+    UARTCharPut(UART0_BASE, 'T');
+    UARTCharPut(UART0_BASE, 'e');
+    UARTCharPut(UART0_BASE, 'x');
+    UARTCharPut(UART0_BASE, 't');
+    UARTCharPut(UART0_BASE, ':');
+    UARTCharPut(UART0_BASE, ' ');
+
+    while(1)
+    {
+        //        if (UARTCharsAvail(UART0_BASE))
+        //        {
+        //            UARTCharPut(UART0_BASE, UARTCharGet(UART0_BASE));
+        //        }
+    }
+}
+
+void UARTIntHandler()
+{
+    uint32_t ui32Status;
+    ui32Status = UARTIntStatus(UART0_BASE, true);
+    UARTIntClear(UART0_BASE, ui32Status);
+    while(UARTCharsAvail(UART0_BASE)) //loop while there are chars
+    {
+        int32_t ucData = UARTCharGet(UART0_BASE);
+        UARTCharPut(UART0_BASE, ucData);
+//        UARTCharPutNonBlocking(UART0_BASE, 0x41);
+        //echo character
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2); //blink LED
+        SysCtlDelay(SysCtlClockGet() / (1000 * 3)); //delay ~1 msec
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0); //turn off LED
+    }
+
+}
