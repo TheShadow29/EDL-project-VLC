db255e317cd85137541337790ce09b0dd0d96666
Arka Sadhu <ark.sadhu2904@gmail.com>
2017-02-13 01:06:32 +0530

    Written Rx code, but couldn't test it completely

--- codes/receiver_code_vlc/tm4c123gh6pm_startup_ccs.c
+++ codes/receiver_code_vlc/tm4c123gh6pm_startup_ccs.c
@@ -41,6 +41,8 @@ static void IntDefaultHandler(void);
 //
 //*****************************************************************************
 extern void _c_int00(void);
+extern void UARTIntHandler();
+extern void PortFIntHandler();
 
 //*****************************************************************************
 //
@@ -88,7 +90,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // GPIO Port C
     IntDefaultHandler,                      // GPIO Port D
     IntDefaultHandler,                      // GPIO Port E
-    IntDefaultHandler,                      // UART0 Rx and Tx
+    UARTIntHandler,                      // UART0 Rx and Tx
     IntDefaultHandler,                      // UART1 Rx and Tx
     IntDefaultHandler,                      // SSI0 Rx and Tx
     IntDefaultHandler,                      // I2C0 Master and Slave
@@ -113,7 +115,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // Analog Comparator 2
     IntDefaultHandler,                      // System Control (PLL, OSC, BO)
     IntDefaultHandler,                      // FLASH Control
-    IntDefaultHandler,                      // GPIO Port F
+    PortFIntHandler,                      // GPIO Port F
     IntDefaultHandler,                      // GPIO Port G
     IntDefaultHandler,                      // GPIO Port H
     IntDefaultHandler,                      // UART2 Rx and Tx
--- codes/receiver_code_vlc/uart_rx_testing.c
+++ codes/receiver_code_vlc/uart_rx_testing.c
@@ -11,7 +11,13 @@
 
 uint8_t character_get[100];
 void send_bin(uint8_t bin);
-void convert_uint_to_bin(int num);
+void convert_char_to_bin(uint8_t ch);
+void send_char();
+void PortFIntHandler();
+uint8_t bool_to_uint8();
+int counter;
+bool character[8];
+uint8_t char_to_send;
 
 int main()
 {
@@ -26,6 +32,14 @@ int main()
 
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_2);
+
+//    Interrupt configurations for PortF0
+//    https://sites.google.com/site/luiselectronicprojects/tutorials/tiva-tutorials/tiva-gpio/digital-input-with-interrupt
+    GPIOIntTypeSet(GPIO_PORTF_BASE,GPIO_PIN_0,GPIO_BOTH_EDGES);
+    GPIOIntRegister(GPIO_PORTF_BASE,PortFIntHandler);
+
+    GPIOIntEnable(GPIO_PORTF_BASE, GPIO_INT_PIN_4);
+
 
     UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet() , 115200,
                         (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
@@ -47,7 +61,7 @@ int main()
     UARTCharPut(UART0_BASE, 't');
     UARTCharPut(UART0_BASE, ':');
     UARTCharPut(UART0_BASE, ' ');
-
+    counter = 0;
     while(1)
     {
         //        if (UARTCharsAvail(UART0_BASE))
@@ -55,6 +69,41 @@ int main()
         //            UARTCharPut(UART0_BASE, UARTCharGet(UART0_BASE));
         //        }
     }
+}
+
+void send_char()
+{
+    UARTCharPut(UART0_BASE, char_to_send);
+}
+
+void PortFIntHandler()
+{
+//    Avoid Infinite Loop
+//    GPIOIntClear(GPIO_PORTF_BASE, GPIO_INT_PIN_0);
+    uint32_t status=0;
+    status = GPIOIntStatus(GPIO_PORTF_BASE,true);
+    GPIOIntClear(GPIO_PORTF_BASE,status);
+    int32_t value = GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4);
+
+    character[counter] = value;
+    counter = counter + 1;
+    if (counter == 8)
+    {
+        counter = 0;
+        char_to_send = bool_to_uint8(character);
+        send_char();
+    }
+}
+
+uint8_t bool_to_uint8()
+{
+    int i =0;
+    uint8_t ch = 0;
+    for (i = 0; i < 8; i++)
+    {
+        ch = ch + (1 << i) * character[i];
+    }
+    return ch;
 }
 
 void UARTIntHandler()
@@ -67,16 +116,41 @@ void UARTIntHandler()
     {
         int32_t ucData = UARTCharGet(UART0_BASE);
         character_get[i++] = ucData;
-        UARTCharPut(UART0_BASE, ucData);
-//        UARTCharPutNonBlocking(UART0_BASE, 0x41);
+        if (ucData == 0x41)
+        {
+                            UARTCharPut(UART0_BASE, ucData);
+
+            convert_char_to_bin(0x42);
+
+            break;
+        }
+        //        UARTCharPutNonBlocking(UART0_BASE, 0x41);
         //echo character
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2); //blink LED
         SysCtlDelay(SysCtlClockGet() / (1000 * 3)); //delay ~1 msec
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0); //turn off LED
     }
-//    convert_uint_to_bin(i);
+    //    convert_to_ascii(i);
+
 }
 
+void convert_char_to_bin(uint8_t ch)
+{
+    int j = 0;
+    for(j = 0; j < 8; j++)
+    {
+        uint8_t b = ch%2;
+        send_bin(b);
+        ch = ch/2;
+    }
+
+}
+
+
+void send_bin(uint8_t bin)
+{
+    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_0,bin);
+}
 
 
 
