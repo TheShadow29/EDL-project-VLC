953c9770c53ce2c6fb1d7342a602f34c22c94831
Arka Sadhu <ark.sadhu2904@gmail.com>
2017-04-06 00:18:20 +0530

    Started Final report

---
+++ Final_report/auto/Final_report.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook
+ "Final_report"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "a4paper" "tmargin=1in" "bmargin=1in") ("inputenc" "utf8")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "geometry"
+    "inputenc"
+    "graphicx"
+    "parskip"
+    "pdflscape"
+    "listings")
+   (TeX-add-symbols
+    "ra"))
+ :latex)
+
